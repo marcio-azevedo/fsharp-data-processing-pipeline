d8fd36aeff557a46acd59cdac8e69cf19360e8aa
Márcio Azevedo <MarcioAzevedo@Marcios-MacBook-Pro.local>
2016-05-03 17:13:13 +0100

    no message

--- src/FSharp.DataProcessingPipelines.Azure.ServiceBus/AssemblyInfo.fs
+++ src/FSharp.DataProcessingPipelines.Azure.ServiceBus/AssemblyInfo.fs
@@ -2,12 +2,12 @@
 open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("FSharp.DataProcessingPipelines.Azure.ServiceBus")>]
-[<assembly: AssemblyProductAttribute("FSharp.DataProcessingPipelines")>]
+[<assembly: AssemblyProductAttribute("FSharp.DataProcessingPipelines.Core")>]
 [<assembly: AssemblyDescriptionAttribute("Provides an extensible solution for creating Data Processing Pipelines in F#.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
-    let [<Literal>] InformationalVersion = "1.0"
+    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] InformationalVersion = "0.1.0"
